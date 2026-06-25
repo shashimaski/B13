989197f32e0f58d0a0239c72f65d9d1b30a24cdc
aksharamol <aksharamol92@gmail.com>
2026-06-25 20:54:15 +0530

    Repository push

--- Terraform/Akshara/provider.tf
+++ Terraform/Akshara/provider.tf
@@ -10,4 +10,5 @@ resource "azurerm_resource_group" "rgdemo" {
 import{
         id= "/subscriptions/a81a1fbd-7f56-4047-86f4-5e06793d5ca9/resourceGroups/cli_grp"
 		to= azurerm_resource_group.rgdemo
+B
 		}
